86881ff9456c8ab280a267ea20533eb35b9d4ba6
Teguhriyadi <ilham.teguh55@gmail.com>
2021-12-27 23:39:38 +0700

    upload login

--- P4M/routes/web.php
+++ P4M/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AppController;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PostController;
 use App\Models\Model\Kategori;
 use Illuminate\Support\Facades\Route;
@@ -22,18 +23,25 @@ Route::get('/', function () {
 });
 
 Route::prefix("page")->group(function() {
-    Route::prefix("admin")->group(function() {
 
-        // Dashboard
-        Route::get("/dashboard", [AppController::class, "dashboard"]);
+    Route::group(["middleware" => ["guest"]], function() {
+        Route::get("/login", [LoginController::class, "login"])->name("login");
+        Route::post("/post_login", [LoginController::class, "post_login"]);
+    });
 
-        // Kategori
-        Route::get("/kategori/checkSlug", [KategoriController::class, "checkSlug"]);
-        Route::resource("/kategori", KategoriController::class);
+    Route::group(["middleware" => ["auth"]], function() {
+        Route::prefix("admin")->group(function() {
+            // Dashboard
+            Route::get("/dashboard", [AppController::class, "dashboard"]);
 
-        // Post
-        Route::resource("/blog", PostController::class);
+            // Kategori
+            Route::get("/kategori/checkSlug", [KategoriController::class, "checkSlug"]);
+            Route::resource("/kategori", KategoriController::class);
 
+            // Post
+            Route::resource("/blog", PostController::class);
+            Route::get("/logout", [LoginController::class, "logout"]);
+        });
     });
 });
 
