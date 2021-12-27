cb40bba499711cd26dcafbcf3a3c3d42111adb94
Teguhriyadi <ilham.teguh55@gmail.com>
2021-12-28 01:54:51 +0700

    upload tampilan frontend

---
+++ P4M/resources/views/pengunjung/layouts/partials/css/style_css.blade.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="{{ url('/frontend') }}/css/academy/style.css">
+<link rel="stylesheet" href="{{ url('/frontend') }}/css/style.css">
+<link rel="stylesheet" href="{{ url('/frontend') }}/vendor/fontawesome-free/css/all.min.css">
+<link rel="stylesheet" href="{{ url('/frontend') }}/vendor/datatables/dataTables.bootstrap4.min.css">
+
+<title>Sistem Informasi Desa Arahan Lor</title>
+
+<link rel="icon" href="{{ url('/frontend') }}/img/logo-kabupaten.png">
+
+<script src="{{ url('/frontend') }}/js/jquery-2.2.4.min.js"></script>
---
+++ P4M/resources/views/pengunjung/layouts/partials/js/style_js.blade.php
@@ -0,0 +1,8 @@
+<script src="{{ url('/frontend') }}/js/popper.min.js"></script>
+<script src="{{ url('/frontend') }}/js/bootstrap.min.js"></script>
+<script src="{{ url('/frontend') }}/js/plugins.js"></script>
+<script src="{{ url('/frontend') }}/js/active.js"></script>
+
+<script src="{{ url('/frontend') }}/vendor/datatables/jquery.dataTables.min.js"></script>
+<script src="{{ url('/frontend') }}/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+<script src="{{ url('/frontend') }}/js/demo/datatables-demo.js"></script>
