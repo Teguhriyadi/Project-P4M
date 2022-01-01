7acca9ae8728bc3c532ef4025e3475d378e54b10
andditafsal <anditasalsabila12@gmail.com>
2022-01-01 19:08:05 +0700

    menambahkan maps

--- P4M/resources/views/pengunjung/page/profil/wilayah.blade.php
+++ P4M/resources/views/pengunjung/page/profil/wilayah.blade.php
@@ -33,6 +33,9 @@
                             </tbody>
                         </table>
                     </div>
+
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15861.119451467082!2d108.26136122408106!3d-6.357809876387517!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6eb9bba6edf671%3A0x51a5177a20cb6fea!2sArahan%20Lor%2C%20Arahan%2C%20Kabupaten%20Indramayu%2C%20Jawa%20Barat!5e0!3m2!1sid!2sid!4v1641038805549!5m2!1sid!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+
                 </div>
             </div>
         </div>
