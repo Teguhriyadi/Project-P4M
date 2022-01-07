75b64ca1950a59f1fcef90ebc77e5bff37b82596
Teguhriyadi <ilham.teguh55@gmail.com>
2022-01-07 23:12:06 +0700

    u

---
+++ P4M/app/Models/Model/JenisSDK.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models\Model;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class JenisSDK extends Model
+{
+    use HasFactory;
+
+    protected $table = "tb_jenis_sdk";
+
+    protected $guarded = [''];
+
+    public $timestamps = false;
+}
