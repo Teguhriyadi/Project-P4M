dc01e996f66cb62b4e53f0a412293cf4aadba047
Teguhriyadi <ilham.teguh55@gmail.com>
2022-01-08 16:22:53 +0700

    membuat migration & seeder data cacat

---
+++ P4M/app/Http/Controllers/CacatController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Model\Cacat;
+use Illuminate\Http\Request;
+
+class CacatController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Model\Cacat  $cacat
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Cacat $cacat)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Model\Cacat  $cacat
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Cacat $cacat)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Model\Cacat  $cacat
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Cacat $cacat)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Model\Cacat  $cacat
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Cacat $cacat)
+    {
+        //
+    }
+}
---
+++ P4M/app/Models/Model/Cacat.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models\Model;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cacat extends Model
+{
+    use HasFactory;
+
+    protected $table = "tb_cacat";
+
+    protected $guarded = [''];
+
+    public $timestamps = false;
+
+}
