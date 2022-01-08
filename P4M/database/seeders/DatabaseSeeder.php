7938292bb5ca2e50c603eb09cb578968603ab659
Teguhriyadi <ilham.teguh55@gmail.com>
2022-01-08 14:59:31 +0700

    revisi seeder

--- P4M/database/seeders/DatabaseSeeder.php
+++ P4M/database/seeders/DatabaseSeeder.php
@@ -18,54 +18,9 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        User::create([
-            "name" => "admin",
-            "username" => "administrator",
-            "email" => "admin@gmail.com",
-            "password" => bcrypt("password"),
-            "hak_akses_id" => 1
-        ]);
-
-        User::create([
-           "name" => "user",
-           "username" => "user",
-           "email" => "user@gmail.com",
-           "password" => bcrypt("password"),
-           "hak_akses_id" => 2
-        ]);
-
-        Kategori::create([
-            "nama" => "Web Programming",
-            "slug" => "web-programming"
-        ]);
-
-        Kategori::create([
-            "nama" => "Web Design",
-            "slug" => "web-design"
-        ]);
-
-        Kategori::create([
-            "nama" => "Programming",
-            "slug" => "programming"
-        ]);
-
-        Kategori::create([
-            "nama" => "Web Data",
-            "slug" => "web-data"
-        ]);
-
-        Kategori::create([
-            "nama" => "Web 123",
-            "slug" => "web-123"
-        ]);
-
-        HakAkses::create([
-            "nama_hak_akses" => "Administrator"
-        ]);
-
-        HakAkses::create([
-            "nama_hak_akses" => "Super Admin"
-        ]);
+        $this->call(UserSeeder::class);
+        $this->call(KategoriSeeder::class);
+        $this->call(HakAksesSeeder::class);
 
         $this->call(JabatanSeeder::class);
         $this->call(PegawaiSeeder::class);
