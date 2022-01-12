7dcef30484b1aca8657ba427b701b551b153e65c
andditafsal <anditasalsabila12@gmail.com>
2022-01-12 13:04:12 +0700

    p

---
+++ P4M/resources/views/pengunjung/page/peta/index.blade.php
@@ -0,0 +1,20 @@
+@extends('pengunjung/layouts/main')
+
+@section('title', '')
+
+@section('page_content')
+
+<div id="printableArea">
+    <h4 class="catg_titile" style="font-family: Oswald"><font color="#FFFFFF">Profil Wilayah Desa Arahan Lor</font></h4>
+    <div class="post_commentbox">
+        <span class="meta_date">
+            <i class="fa fa-user"></i>Administrator&nbsp;
+            <i class="fa fa-eye"></i>0 Kali Dibaca&nbsp;
+        </span>
+    </div>
+    <div class="single_page_content" style="margin-bottom:10px;">
+
+    </div>
+</div>
+
+@endsection
