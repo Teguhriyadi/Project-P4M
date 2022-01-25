b93f075c052b00f60741d6aa2310f2090db5f24c
Teguhriyadi <ilham.teguh55@gmail.com>
2022-01-25 11:54:33 +0700

    select2 di rw

--- P4M/resources/views/admin/page/penduduk/rt/edit_data_rt.blade.php
+++ P4M/resources/views/admin/page/penduduk/rt/edit_data_rt.blade.php
@@ -1,7 +1,8 @@
 <input type="hidden" name="id" value="{{ $edit->id }}">
 <div class="form-group">
     <label for="id_rw">RW</label>
-    <select class="form-control" name="id_rw" id="id_rw">
+    <select class="form-control select2" name="id_rw" id="id_rw">
+        <option value="">- Silahkan cari RW -</option>
         @foreach ($data_rw as $rw)
             <option value="{{ $rw->id }}" {{ $rw->id == $edit->id_rw ? 'selected' : '' }}>{{ $rw->rw }}</option>
         @endforeach
@@ -11,3 +12,8 @@
     <label for="rt"> RT </label>
     <input type="text" class="form-control" name="rt" id="rt" placeholder="Masukkan RT" value="{{ $edit->rt }}">
 </div>
+<script>
+    $(function() {
+        $(".select2").select2()
+    });
+</script>
