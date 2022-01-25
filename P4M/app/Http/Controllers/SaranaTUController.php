22c641e661fbf8d6b1edb27c7eede285f2d6ffef
Teguhriyadi <ilham.teguh55@gmail.com>
2022-01-26 01:53:18 +0700

    push

--- P4M/app/Http/Controllers/SaranaTUController.php
+++ P4M/app/Http/Controllers/SaranaTUController.php
@@ -7,57 +7,35 @@ use Illuminate\Http\Request;
 
 class SaranaTUController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $tempat_usaha = SaranaTempatUsaha::all();
         return view('admin.page.sarana.tu.index', compact('tempat_usaha'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        SaranaTempatUsaha::create($request->all());
+        $cek = SaranaTempatUsaha::where("nama", $request->nama)->count();
 
-        return back()->with('message', "<script>swal('Selamat!', 'Data Berhasil di Tambahkan', 'success')</script>");
+        if ($cek) {
+            return redirect("/page/admin/sarana/tempat-usaha")->with('message', "<script>swal('Oops', 'Tidak Boleh Duplikasi Data', 'error');</script>");
+        } else {
+            SaranaTempatUsaha::create($request->all());
+
+            return back()->with('message', "<script>swal('Selamat!', 'Data Berhasil di Tambahkan', 'success')</script>");
+        }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Request $request)
     {
         $edit = SaranaTempatUsaha::where('id', $request->id)->first();
@@ -65,13 +43,6 @@ class SaranaTUController extends Controller
         return view('admin.page.sarana.tu.edit', compact('edit'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request)
     {
         SaranaTempatUsaha::where('id', $request->id)->update([
@@ -83,12 +54,6 @@ class SaranaTUController extends Controller
         return back()->with('message', "<script>swal('Selamat!', 'Data Berhasil di Ubah', 'success')</script>");
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         SaranaTempatUsaha::where('id', $id)->delete();
