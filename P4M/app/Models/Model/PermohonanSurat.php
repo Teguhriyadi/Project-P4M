b99abf2913314c4636796d059be69b4892644fc8
andditafsal <anditasalsabila12@gmail.com>
2022-01-27 23:29:30 +0700

    edit format surat

--- P4M/app/Models/Model/PermohonanSurat.php
+++ P4M/app/Models/Model/PermohonanSurat.php
@@ -12,4 +12,14 @@ class PermohonanSurat extends Model
     protected $table = 'tb_permohonan_surat';
 
     protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    public function getPenduduk()
+    {
+        return $this->hasOne(Penduduk::class, 'id', 'nik');
+    }
+
+    public function getSurat()
+    {
+        return $this->hasOne(SuratFormat::class, 'id', 'id_surat');
+    }
 }
--- P4M/routes/web.php
+++ P4M/routes/web.php
@@ -153,6 +153,7 @@ Route::prefix('/data')->group(function () {
 
 Route::get('/kependudukan', [UserController::class, 'kependudukan']);
 Route::get('/data/penduduk', [UserController::class, 'penduduk']);
+Route::get('/penduduk/json', [SuratOnlineController::class, 'penduduk']);
 Route::resource('/surat', SuratOnlineController::class);
 
 Route::post('/komentar/{slug}', function (Request $request, $slug) {
