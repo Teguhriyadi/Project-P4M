61047cc73d61149a1712c54a93997b9bf89bb583
andditafsal <anditasalsabila12@gmail.com>
2022-01-28 11:22:07 +0700

    push

--- P4M/resources/views/admin/page/permohonan_surat/index.blade.php
+++ P4M/resources/views/admin/page/permohonan_surat/index.blade.php
@@ -1,6 +1,6 @@
 @extends('admin.layouts.main')
 
-@section('title', 'Permohonan Surat')
+@section('title', 'Data Permohonan Surat')
 
 @section('page_content')
 
@@ -9,7 +9,11 @@
         @yield('title')
     </h1>
     <ol class="breadcrumb">
-        <li><a href="/page/admin">Dashboard</a></li>
+        <li>
+            <a href="{{ url('/page/admin/dashboard') }}">
+                <i class="fa fa-home"></i> Home
+            </a>
+        </li>
         <li class="active">@yield('title')</li>
     </ol>
 </section>
@@ -17,7 +21,7 @@
 <section class="content">
     <div class="row">
         <div class="col-md-12">
-            <div class="box">
+            <div class="box box-info">
                 <div class="box-header">
                     <h3 class="box-title">
                         <i class="fa fa-users"></i> @yield('title')
@@ -30,24 +34,26 @@
                         <table id="example1" class="table table-bordered table-striped" style="width: 100%">
                             <thead>
                                 <tr>
-                                    <th class="">No.</th>
+                                    <th class="text-center">No.</th>
                                     <th class="">Nama</th>
                                     <th class="">NIK</th>
-                                    <th class="">Jenis Surat</th>
-                                    <th class="">Telepon</th>
-                                    <th class="">Aksi</th>
+                                    <th class="text-center">Jenis Surat</th>
+                                    <th class="text-center">Telepon</th>
+                                    <th class="text-center">Aksi</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 @foreach ($pemohon as $p)
                                 <tr>
-                                    <th>{{ $loop->iteration }}</th>
+                                    <td class="text-center">{{ $loop->iteration }}</td>
                                     <td>{{ $p->getPenduduk->nama }}</td>
                                     <td>{{ $p->getPenduduk->nik }}</td>
                                     <td>{{ $p->getSurat->nama }}</td>
-                                    <td>{{ $p->telepon }}</td>
-                                    <td>
-                                        <a href="{{ url('page/admin/cetak_surat/form/'.$p->getSurat->url_surat.'/'.$p->nik) }}" class="btn bg-purple"><i class="fa fa-file-word-o"></i></a>
+                                    <td class="text-center">{{ $p->telepon }}</td>
+                                    <td class="text-center">
+                                        <a href="{{ url('page/admin/cetak_surat/form/'.$p->getSurat->url_surat.'/'.$p->nik) }}" class="btn bg-purple" title="Cetak Surat">
+                                            <i class="fa fa-file-word-o"></i>
+                                        </a>
                                     </td>
                                 </tr>
                                 @endforeach
